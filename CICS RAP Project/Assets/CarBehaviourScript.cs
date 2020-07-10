7212797be61b5ce287d89e3828cb54e88fcec416
StuffGames <44318828+StuffGames@users.noreply.github.com>
2020-07-10 18:28:25 -0400

    Project Commit
    This is the start of the project and restart of this repository lol

---
+++ CICS RAP Project/Assets/CarBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviourScript : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float turnSpeed = 5;
+    public int speed = 5;
+    private float moveY;
+    private float turnX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveY = Input.GetAxis("Vertical");
+        turnX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(rb.velocity.x, moveY * speed);
+        transform.Rotate(Vector3.forward, turnX * turnSpeed);
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
