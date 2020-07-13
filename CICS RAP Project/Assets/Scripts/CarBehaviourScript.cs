93f0a2bc55aaa86b4016e14a6ff15a0ad79f48a1
Joshua Shen <joshshen118@gmail.com>
2020-07-12 21:29:36 -0400

    race track, camera follow
    I put in a race track as well as a script for the camera to follow the car. I also imported the car sprite that Jasmine made. We should definitely make our own race track at some point because this one is not very good, but I just put this one in for now to see how it looks with my camera follow.

---
+++ CICS RAP Project/Assets/Scripts/CarBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviourScript : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float turnSpeed = 5;
+    public int speed = 6;
+    private float moveY;
+    private float turnX;
+    private float spin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveY = Input.GetAxis("Vertical");
+        turnX = Input.GetAxis("Horizontal");
+        rb.velocity = transform.up * (moveY * speed);
+        spin = turnX * -turnSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb.velocity.x != 0 || rb.velocity.y != 0)
+        {
+            transform.Rotate(Vector3.forward, spin);
+        } 
+    }
+}
