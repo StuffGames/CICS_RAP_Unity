f3165aae57202096f418d12fd670a842e7a03d71
StuffGames <44318828+StuffGames@users.noreply.github.com>
2020-07-28 02:46:35 -0400

    Updated Scripts, Assets Implemented
    Not as many changed files. Let's hope this push doesn't mess things up.
    - Car turning changed to using torque so it feels more natural and smooth. It works so far but it is very slow, sluggish, and needs more tweeking around.
    - Got forward velocity and turning fixated around that.
    - Experimenting with Add Force shows some promise although it is very incosistent and out of control.
    
    - Updated Josh's script to implement the Slerp function for a more smooth camera tracking system.
    
    - Added the car asset and had it pivot around the front wheels.
    - Added modified race track as a nice background
    
    Up Next:
    - Make camera smoother because it moves smoothly but not. It's jittery while trying to follow the car and idk if its the camera or the way the car moves.
    - Keep working on the movement because it is not natural yet, the forces were a good step but nothing concrete. Velocity is too sharp and whilst i kinda want to work on like a modified velocity, i also want to just switch to forces.
    - Fix torque turning because it slows down but in a strange way and doesn't feel too controlable.
    - Get the tire asset and work on the tire turning idea i had in mind because it seems cool :)
    - Finalize car movement before moving onto like other mechanics but that's more stuff in the future.
    - i cant think of anything else lmao im tired

---
+++ CICS RAP Project/Assets/Scripts/CarBehaviourScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBehaviourScript : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float turnSpeed = 5;
+    public int speed = 6;
+    private float moveY;
+    private float turnX;
+    private float spin;
+    private float fVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveY = Input.GetAxis("Vertical");
+        turnX = Input.GetAxis("Horizontal");
+        spin = turnX * -turnSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        fVelocity = transform.InverseTransformDirection(rb.velocity).y;
+
+        if (fVelocity > 0.1)
+        {
+            //transform.Rotate(Vector3.forward, spin);
+            rb.AddTorque(-turnX * turnSpeed);
+        }
+        
+        rb.velocity = transform.up * (moveY * speed);
+
+        /*
+         * THIS SORTA WORKS IN A WAY, if we move foward with velocity insteam then just delete this pls
+        rb.AddForce(transform.up * (moveY * speed));
+        */
+
+    }
+}
---
+++ CICS RAP Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+    public float speed = 0.05f;
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.Slerp(transform.position, player.transform.position + offset, speed);
+    }
+}
